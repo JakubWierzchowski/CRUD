790ed6321370740d59f0a889177e0cc904972f1c
Jakub Wierzchowski <wierzcho155@gmai.com>
2022-04-07 19:45:03 +0200

    final2

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+const firebaseConfig = {};
 
 export const app = initializeApp(firebaseConfig);
 export default getFirestore();
