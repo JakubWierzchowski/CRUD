91d247eb0f46e70abde7428ef3e2cafbd1e48a83
Jakub Wierzchowski <wierzcho155@gmai.com>
2022-05-23 11:00:29 +0200

    reducer

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAX_c5Sq5e10XoIXlpHspQLdOYAS7uyquU",
+  authDomain: "rekrutacja-97060.firebaseapp.com",
+  projectId: "rekrutacja-97060",
+  storageBucket: "rekrutacja-97060.appspot.com",
+  messagingSenderId: "109216656502",
+  appId: "1:109216656502:web:970ad907e6e92be0f04357",
+  measurementId: "G-4SXL7XK4RB",
+};
 
 export const app = initializeApp(firebaseConfig);
 export default getFirestore();
